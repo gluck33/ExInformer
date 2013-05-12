e34cbf8c6ad7f7c0c6182567baf9ae8b78fb7e33
oleg <gluck33@gmail.com>
2013-05-12 22:23:48 +0400

    Класс сервис запущен. Доделать взаиодействие с главной активити.

---
+++ ExInformer/src/ru/openitr/exinformer/InfoRefreshReciever.java
@@ -0,0 +1,11 @@
+package ru.openitr.exinformer;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: oleg
+ * Date: 11.05.13
+ * Time: 12:55
+ * To change this template use File | Settings | File Templates.
+ */
+public class InfoRefreshReciever {
+}
