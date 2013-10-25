258c1da670eb54ff953294aa2071e6eff852eaa3
oleg <gluck33@gmail.com>
2013-10-25 13:13:14 +0400

    ProgressDialog changet to Crossfade.

--- ExInformer/src/ru/openitr/cbrfinfo/MainActivity.java
+++ ExInformer/src/ru/openitr/cbrfinfo/MainActivity.java
@@ -1,6 +1,8 @@
 package ru.openitr.cbrfinfo;
 
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.app.DatePickerDialog;
@@ -64,11 +66,20 @@ public class MainActivity extends FragmentActivity {
     Intent refreshServiceIntent;
     boolean onDateSet;
 
+    //****************************
+    private View mContentView;
+    private View mLoadingView;
+    private int mShortAnimationDuration;
+
+    //****************************
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContentView = findViewById(R.id.pager);
+        mLoadingView = findViewById(R.id.loading_spinner);
+        mLoadingView.setVisibility(View.GONE);
         onDate = Calendar.getInstance();
         notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         fragments.add(CURRENCY_FRAGMENT, new CurrencyInfoFragment());
@@ -93,10 +104,13 @@ public class MainActivity extends FragmentActivity {
                 return pagesTitles[position];
             }
         };
+        //******************************
+        //*****************************
         viewPager = (ViewPager) findViewById(R.id.pager);
         viewPager.setAdapter(fragmentPagerAdapter);
         viewPager.setCurrentItem(CURRENCY_FRAGMENT);
         setInfoDateToTitle();
+
     }
 
     @Override
@@ -251,6 +265,40 @@ public class MainActivity extends FragmentActivity {
         }
     };
 
+    private void beginProgress(){
+        mContentView.setVisibility(View.GONE);
+        mLoadingView.setVisibility(View.VISIBLE);
+        mShortAnimationDuration = getResources().getInteger(
+        android.R.integer.config_shortAnimTime);
+    }
+
+    private void endProgress() {
+        // Set the content view to 0% opacity but visible, so that it is visible
+        // (but fully transparent) during the animation.
+        mContentView.setAlpha(0f);
+        mContentView.setVisibility(View.VISIBLE);
+
+        // Animate the content view to 100% opacity, and clear any animation
+        // listener set on the view.
+        mContentView.animate()
+                .alpha(1f)
+                .setDuration(mShortAnimationDuration)
+                .setListener(null);
+
+        // Animate the loading view to 0% opacity. After the animation ends,
+        // set its visibility to GONE as an optimization step (it won't
+        // participate in layout passes, etc.)
+        mLoadingView.animate()
+                .alpha(0f)
+                .setDuration(mShortAnimationDuration)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        mLoadingView.setVisibility(View.GONE);
+                    }
+                });
+    }
+
     /**
      *
      */
@@ -268,8 +316,9 @@ public class MainActivity extends FragmentActivity {
                 switch (status) {
                     case STATUS_BEGIN_REFRESH:
                         LogSystem.logInFile(LOG_TAG, this.getClass().getSimpleName() + " : Begin updating info.");
-                        progressDialog = AppDialog.newInstance(AppDialog.PROGRESS_DIALOG);
-                        progressDialog.show(getSupportFragmentManager(), "");
+//                        progressDialog = AppDialog.newInstance(AppDialog.PROGRESS_DIALOG);
+//                        progressDialog.show(getSupportFragmentManager(), "");
+                        beginProgress();
                         break;
                     case FIN_STATUS_NO_DATA:
                         progressDialog.dismiss();
@@ -300,8 +349,9 @@ public class MainActivity extends FragmentActivity {
                     default:
                         LogSystem.logInFile(LOG_TAG, this.getClass().getSimpleName() + " : Refreshing OK.");
                         setInfoDateToTitle();
-                        if (progressDialog != null)
-                            progressDialog.dismiss();
+                        endProgress();
+//                        if (progressDialog != null)
+//                            progressDialog.dismiss();
                         break;
                 }
             }
