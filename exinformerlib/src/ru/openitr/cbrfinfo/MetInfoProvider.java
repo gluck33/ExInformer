92669ece2b50cf079136bfcbe359154b7b299874
oleg <gluck33@gmail.com>
2013-10-31 13:23:26 +0400

    Добавлен класс MetInfoProvider - провайдер для таблицы металлов.

---
+++ exinformerlib/src/ru/openitr/cbrfinfo/MetInfoProvider.java
@@ -0,0 +1,209 @@
+package ru.openitr.cbrfinfo;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.net.Uri;
+import android.text.TextUtils;
+
+/**
+ * Created by
+ * User: oleg
+ * Date: 01.04.13
+ * Time: 11:06
+ */
+public class MetInfoProvider extends ContentProvider {
+    // Имена столбцов
+    public static final String KEY_ID = "_id";
+    public static final String KEY_CODE = "mCode";
+    public static final String KEY_PRICE = "mPrice";
+    public static final String KEY_DATE = "mDate";
+    private static final String KEY_IMAGE_URI = "mImageUri";
+    public static final String KEY_VISIBLE = "vVisible";
+    public static final String KEY_ORDER = "vOrder";
+
+    // Индексы столбцов
+
+    public static final int ID_COL_NUM = 0;
+    public static final int CODE_COL_NUM = 1;
+    public static final int PRICE_COL_NUM = 2;
+    public static final int DATE_COL_NUM = 3;
+    public static final int IMAGE_COL_NUM = 4;
+
+    // База данных
+
+    public static final String DATABASE_NAME = "exInformer.db";
+    public static String METAL_TABLE = "metaltable";
+    public static final int DATABASE_VERSION = 4;
+
+    // Скрипт создания
+
+    protected static final String CREATE_METALL_TABLE = "create table " +
+            METAL_TABLE + " (" +
+            KEY_ID + " integer primary key autoincrement, " +
+            KEY_CODE + " ineger, " +
+            KEY_PRICE + " real," +
+            KEY_DATE + " long, " +
+            KEY_IMAGE_URI + " text," +
+            KEY_VISIBLE + " integer," +
+            KEY_ORDER + " integer" +
+            ");";
+
+
+    SQLiteDatabase db;
+    //URI
+        static final String AUTHORITY = "ru.openitr.cbrfinfo.metal";
+    //PATH
+    static final String METAL_PATH = "metals";
+    //Общий URI
+    public static final Uri METAL_CONTENT_URI = Uri.parse("content://"+AUTHORITY+"/"+ METAL_PATH);
+    // Типы данных
+    // набор строк
+    static final String METAL_CONTENT_TYPE = "vnd.android.cursor.dir/vnd." + AUTHORITY + "." + METAL_PATH;
+    // Одна строка
+    static final String METAL_CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd." + AUTHORITY + "." + METAL_PATH;
+    // UriMatcher
+    // общий Uri
+    static final int URI_METALL = 1;
+    // Uri с указанным ID
+    static final int URI_METAL_ID = 2;
+
+    private static final UriMatcher uriMatcher;
+
+    static {
+        uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI(AUTHORITY, METAL_PATH, URI_METALL);
+        uriMatcher.addURI(AUTHORITY, METAL_PATH +"/*", URI_METAL_ID);
+    }
+    DBHelper dbHelper;
+    @Override
+    public boolean onCreate() {
+        dbHelper = new DBHelper(getContext());
+        return true;
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        db.close();
+        super.finalize();
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        switch (uriMatcher.match(uri)) {
+            case URI_METALL:
+//                if (TextUtils.isEmpty(sortOrder)) {
+//                    sortOrder = CurrencyDbAdapter.KEY_ORDER + " ASC";
+//                }
+                break;
+
+            case URI_METAL_ID:
+                String mcode = uri.getLastPathSegment();
+                if (TextUtils.isEmpty(selection)) {
+                    selection = selection + " AND " + KEY_CODE +" = " + mcode;
+                }
+                break;
+            default:
+                throw new IllegalArgumentException("Wrong URI: "+uri);
+
+        }
+        db = dbHelper.getReadableDatabase();
+        Cursor cursor = db.query(METAL_TABLE,projection, selection, selectionArgs,null,null,sortOrder);
+        cursor.setNotificationUri(getContext().getContentResolver(), METAL_CONTENT_URI);
+        return cursor;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        switch (uriMatcher.match(uri)){
+            case URI_METALL:
+                return METAL_CONTENT_TYPE;
+            case URI_METAL_ID:
+                return METAL_CONTENT_ITEM_TYPE;
+        }
+        return null;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues _cv){
+        if (uriMatcher.match(uri) != URI_METALL) {
+            throw new IllegalArgumentException("Wrong URI: " + uri);
+        }
+        db = dbHelper.getWritableDatabase();
+        long rowId = db.insert(METAL_TABLE, null,_cv);
+        Uri resultUri = ContentUris.withAppendedId(METAL_CONTENT_URI, rowId);
+//        getContext().getContentResolver().notifyChange(resultUri,null);
+        return resultUri;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        switch (uriMatcher.match(uri)) {
+            case URI_METALL:
+                break;
+            case URI_METAL_ID:
+                String mcode = uri.getLastPathSegment();
+                if (TextUtils.isEmpty(selection)) {
+                    selection = selection + " AND " + KEY_CODE +" = " + mcode;
+                }
+                break;
+            default:
+                throw new IllegalArgumentException("Wrong URI: "+uri);
+
+        }
+        db = dbHelper.getWritableDatabase();
+        int id = db.delete(METAL_TABLE,selection, selectionArgs);
+        return id;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues contentValues, String selection, String[] selectionArgs) {
+        switch (uriMatcher.match(uri)){
+            case URI_METALL:
+                break;
+            case URI_METAL_ID:
+                String _vchCode = uri.getLastPathSegment();
+                if (TextUtils.isEmpty(selection)){
+                  selection = CurrencyDbAdapter.KEY_CHARCODE + " = " + "'" +_vchCode + "'";
+                } else {
+                  selection = selection + " AND " + CurrencyDbAdapter.KEY_CHARCODE + " = " + _vchCode;
+                }
+            break;
+            default:
+                throw new IllegalArgumentException("Wrong URI: "+ uri);
+        }
+        db = dbHelper.getWritableDatabase();
+        int result = db.update(METAL_TABLE, contentValues, selection, selectionArgs);
+        db.close();
+        return result;
+    }
+private class DBHelper extends SQLiteOpenHelper {
+
+    public DBHelper(Context context) {
+        super(context, CurrencyDbAdapter.DATABASE_NAME, null, CurrencyDbAdapter.DATABASE_VERSION);
+    }
+
+    @Override
+    public synchronized void close() {
+        super.close();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_METALL_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i2) {
+        db.execSQL("drop table if exists "+CurrencyDbAdapter.CURRENCY_TABLE);
+        onCreate(db);
+    }
+}
+
+
+}
